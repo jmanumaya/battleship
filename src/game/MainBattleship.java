b130693f675b06a594e6dcc9fe45e7a104f0d389
[name] <[jm.maya@iesnervion.es]>
2025-03-18 13:27:18 +0100

    yes

--- src/game/MainBattleship.java
+++ src/game/MainBattleship.java
@@ -1,9 +1,34 @@
 package game;
 
+import java.util.Scanner;
+
 public class MainBattleship {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		
+		Battleship game = new Battleship();
+		
+		char pos1 = ' ';
+		
+		int pos2;
+		
+		System.out.println("Wellcome to Battleship Game");
+		
+		System.out.println("First! place your ships in the table (you have 4 ships)");
+		
+		do {
+			
+			System.out.println("This is your table:");
+			game.
+			
+			System.out.println("Where you place a ship?:");
+			System.out.println("X: ");
+			pos1 = sc.next().charAt(0);
+			
+			
+		} while();
 
 	}
 
