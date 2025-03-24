9291433e7a1288d7296f30b4e0f24ccb47db7c99
ll.rodriguez <ll.rodriguez@106-15.info2>
2025-03-24 10:40:26 +0100

    rellenar y colocar

--- src/game/MainBattleship.java
+++ src/game/MainBattleship.java
@@ -1,10 +1,29 @@
 package game;
 
+import java.util.Arrays;
+
 public class MainBattleship {
 
 	public static void main(String[] args) {
 		
 
 	}
-
+	
+	public boolean placeShips(char tablero[][], char row, int col) {
+		boolean done = false;
+		int rowNum = row - 'A';
+		if (tablero[rowNum][col-1] != 'B') {
+			tablero[rowNum][col-1] = 'B';
+			done = true;
+		}
+		return done;
+	}
+	
+	public char[][] initializeBoar(int rows, int cols) {
+		char t[][] = new char[rows][cols];
+		for (int i = 0; i < t.length; i++) {
+			Arrays.fill(t[i], '-');
+		}
+		return t;
+	}
 }
