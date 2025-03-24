23a579558505cd4ae087c270796602705ab6461c
jemunoz <jemunoz@106-18.info2>
2025-03-24 10:47:12 +0100

    Paint Table

--- src/game/Battleship.java
+++ src/game/Battleship.java
@@ -2,4 +2,37 @@ package game;
 
 public class Battleship {
 
+	
+	
+	
+	public static void tablePaint(char[][] table) {
+
+		char letterA = 'A';
+
+		for (int i = 1; i <= table.length; i++) {
+
+			System.out.print("\t" + i);
+
+		}
+		
+		System.out.println();
+
+		for (int i = 0; i <= table.length; i++) {
+
+			System.out.print((char) (letterA + i) + "\t");
+			
+			
+			for (int j = 0; j < table.length; j++) {
+
+				System.out.print(table);
+				
+				
+			}
+			
+			System.out.println();
+
+		}
+
+	}
+
 }
--- src/game/MainBattleship.java
+++ src/game/MainBattleship.java
@@ -4,7 +4,13 @@ public class MainBattleship {
 
 	public static void main(String[] args) {
 		
-
+		char[][] table = new char[10][10];
+		
+		Battleship.tablePaint(table);
+		
+		
 	}
+
+
 
 }
