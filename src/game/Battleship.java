1cdd1bfbfde2c635194579a2e406cd49a044eb29
jmanumaya <jm.maya@iesnervion.es>
2025-04-21 00:12:50 +0200

    diría que terminado pero revisar en clase con los compas

--- src/game/Battleship.java
+++ src/game/Battleship.java
@@ -5,288 +5,271 @@ import java.util.Random;
 
 public class Battleship {
 
-	// Constants for board elements
-	private static final char WATER = '-';
-	private static final char SHIP = 'S';
-	private static final char HIT = 'X';
-	private static final char MISS = 'O';
+    // Constants for board elements
+    private static final char WATER = '~';
+    private static final char SHIP = 'S';
+    private static final char HIT = 'X';
+    private static final char MISS = 'W';
 
-	// Game boards for player and machine
-	private static char machineBoard1[][] = new char[10][10];  // COLOCAR BARCOS Y AGUA
-	private static char machineBoard2[][] = new char[10][10];  // jugar
+    // Game boards for player and machine
+    private static char machineBoard1[][] = new char[10][10];  // Board with ships and water
+    private static char machineBoard2[][] = new char[10][10];  // Player's view when playing (shooting)
 
-	private static char playerBoard1[][] = new char[10][10];  // COLOCAR BARCOS Y AGUA
-	private static char playerBoard2[][] = new char[10][10];  // JUGAR
+    private static char playerBoard1[][] = new char[10][10];  // Board with ships and water
+    private static char playerBoard2[][] = new char[10][10];  // Machine's view when playing (shooting)
 
+    public static char[][] getMachineBoard1() {
+        return machineBoard1;
+    }
 
-	public static char[][] getMachineBoard1() {
-		return machineBoard1;
-	}
+    public static void setMachineBoard1(char[][] machineBoard1) {
+        Battleship.machineBoard1 = machineBoard1;
+    }
 
-	public static void setMachineBoard1(char[][] machineBoard1) {
-		Battleship.machineBoard1 = machineBoard1;
-	}
+    public static char[][] getMachineBoard2() {
+        return machineBoard2;
+    }
 
-	public static char[][] getMachineBoard2() {
-		return machineBoard2;
-	}
+    public static void setMachineBoard2(char[][] machineBoard2) {
+        Battleship.machineBoard2 = machineBoard2;
+    }
 
-	public static void setMachineBoard2(char[][] machineBoard2) {
-		Battleship.machineBoard2 = machineBoard2;
-	}
+    public static char[][] getPlayerBoard1() {
+        return playerBoard1;
+    }
 
-	public static char[][] getPlayerBoard1() {
-		return playerBoard1;
-	}
+    public static void setPlayerBoard1(char[][] playerBoard1) {
+        Battleship.playerBoard1 = playerBoard1;
+    }
 
-	public static void setPlayerBoard1(char[][] playerBoard1) {
-		Battleship.playerBoard1 = playerBoard1;
-	}
+    public static char[][] getPlayerBoard2() {
+        return playerBoard2;
+    }
 
-	public static char[][] getPlayerBoard2() {
-		return playerBoard2;
-	}
+    public static void setPlayerBoard2(char[][] playerBoard2) {
+        Battleship.playerBoard2 = playerBoard2;
+    }
 
-	public static void setPlayerBoard2(char[][] playerBoard2) {
-		Battleship.playerBoard2 = playerBoard2;
-	}
+    /**
+     * Prints the game board in a structured format.
+     *
+     * @param table The board to print.
+     */
+    public static void tablePaint(char[][] table) {
+        char letterA = 'A';
 
-	/**
-	 * Prints the game board in a structured format.
-	 *
-	 * @param table The board to print.
-	 */
-	public static void tablePaint(char[][] table) {
+        // Print column numbers
+        System.out.print("\t");
+        for (int i = 1; i <= table[0].length; i++) {
+            System.out.print(i + "\t");
+        }
+        System.out.println();
 
-		char letterA = 'A';
+        // Print rows with letter labels
+        for (int i = 0; i < table.length; i++) {
+            System.out.print((char) (letterA + i) + "\t");
 
-		// Print column numbers
-		for (int i = 1; i <= table.length; i++) {
-			System.out.print("\t" + i);
-		}
-		System.out.println();
+            for (int j = 0; j < table[i].length; j++) {
+                System.out.print(table[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
 
-		// Print rows with letter labels
-		for (int i = 0; i < table.length; i++) {
-			System.out.print((char) (letterA + i) + "\t");
+    /**
+     * Initializes the game board by filling it with water ('-').
+     *
+     * @param table The board to initialize.
+     */
+    public static void initializeBoardWater(char[][] table) {
+        for (int i = 0; i < table.length; i++) {
+            Arrays.fill(table[i], WATER);
+        }
+    }
+    
+    public static void initializeBoardLines(char[][] table) {
+        for (int i = 0; i < table.length; i++) {
+            Arrays.fill(table[i], '-');
+        }
+    }
 
-			for (int j = 0; j < table.length; j++) {
-				System.out.print(table[i][j] + "\t");
-			}
-			System.out.println();
-		}
-	}
+    /**
+     * Randomly places a given number of ships on the board.
+     * Also ensures ships are not placed in adjacent cells.
+     *
+     * @param board    The game board where ships will be placed.
+     * @param numBoats The number of ships to be placed.
+     */
+    public static void generateBoats(char[][] board, int numBoats) {
+        Random rand = new Random();
+        int count = 0;
 
-	/**
-	 * Initializes the game board by filling it with water ('-').
-	 *
-	 * @param table The board to initialize.
-	 */
-	public static void initializeBoardWater(char[][] table) {
-		for (int i = 0; i < table.length; i++) {
-			Arrays.fill(table[i], WATER);
-		}
-	}
-	
-	public static void initializeBoardLines(char[][] table) {
-		for (int i = 0; i < table.length; i++) {
-			Arrays.fill(table[i], '-');
-		}
-	}
+        while (count < numBoats) {
+            int row = rand.nextInt(board.length);
+            int col = rand.nextInt(board[0].length);
 
-	/**
-	 * Randomly places a given number of ships on the board.
-	 *
-	 * @param board    The game board where ships will be placed.
-	 * @param numBoats The number of ships to be placed.
-	 */
-	public static void generateBoats(char[][] board, int numBoats) {
-		Random rand = new Random();
-		int count = 0;
+            // Convert row index to character to use checksPosition method
+            char rowChar = (char)('A' + row);
+            
+            // Check if position is valid (not adjacent to another ship)
+            if (checksPosition(rowChar, col + 1, board)) {
+                board[row][col] = SHIP;
+                count++;
+            }
+        }
+    }
 
-		while (count < numBoats) {
-			int row = rand.nextInt(board.length);
-			int col = rand.nextInt(board[0].length);
+    /**
+     * Simulates the machine's move by randomly selecting a cell on the player's
+     * board. Marks the board with 'X' if a ship is hit, 'O' if missed.
+     *
+     * @return true if a ship is hit, false otherwise.
+     */
+    public static boolean machineMove() {
+        Random rand = new Random();
+        int row;
+        int col;
+        boolean res = false;
 
-			// Place ship only if the cell is empty
-			if (board[row][col] == WATER) {
-				board[row][col] = SHIP;
-				count++;
-			}
-		}
-	}
+        do {
+            row = rand.nextInt(playerBoard1.length);
+            col = rand.nextInt(playerBoard1[0].length);
+        } while (playerBoard2[row][col] == HIT || playerBoard2[row][col] == MISS); // Avoid attacking the same place
 
-	/**
-	 * Simulates the machine's move by randomly selecting a cell on the player's
-	 * board. Marks the board with 'X' if a ship is hit, 'O' if missed.
-	 *
-	 * @param playerBoard The board where the machine attacks.
-	 * @return true if a ship is hit, false otherwise.
-	 */
-	public static boolean machineMove() {
-		Random rand = new Random();
-		int row;
-		int col;
-		boolean res = false;
+        if (playerBoard1[row][col] == SHIP) {
+            // Mark as hit on both boards
+            playerBoard2[row][col] = HIT;
+            playerBoard1[row][col] = HIT;
+            res = true; // Ship hit
+            System.out.println("Computer shot at " + (char)('A' + row) + (col + 1) + " and HIT!");
+        } else {
+            // Mark miss on both boards
+            playerBoard2[row][col] = MISS;
+            playerBoard1[row][col] = MISS;  // Aquí está la corrección - también marcamos el fallo en playerBoard1
+            System.out.println("Computer shot at " + (char)('A' + row) + (col + 1) + " and MISSED!");
+        }
+        
+        return res;
+    }
+    
+    /**
+     * Checks if a position is valid for placing a ship.
+     * A position is valid if it's within bounds and there are no ships in adjacent cells.
+     *
+     * @param letter Row letter
+     * @param col Column number (1-based)
+     * @param table The board to check
+     * @return true if position is valid, false otherwise
+     */
+    public static boolean checksPosition(char letter, int col, char[][] table) {
+        int row = letter - 'A';
+        col -= 1; // Convert to 0-based indexing
 
-		do {
-			row = rand.nextInt(playerBoard2.length);
-			col = rand.nextInt(playerBoard2[0].length);
-		} while (playerBoard2[row][col] == HIT || playerBoard2[row][col] == MISS); // Avoid attacking the same place
-
-		if (playerBoard2[row][col] == SHIP) {
-			playerBoard2[row][col] = HIT;
-			res = true; // Ship hit
-		} else {
-			playerBoard2[row][col] = MISS;
-		}
-		
-		return res;
-		
-	}
-	
-	public boolean checksPosition(char letter, int posJ, char[][] table) {
-		boolean valid = false;
-		int posI = letter - 'A';
-		posJ -= 1;
-
-		// If the position is between the limits and there isn't a ship on the position,
-		// the position is valid
-		if ((posI >= 0 && posI < table.length) && (posJ >= 0 && posJ < table[posJ].length)
-				&& table[posI][posJ] != 'S') {
-			valid = true;
-		}
-
-		// We check the adjacent positions
-
-		// Check if the adjacent position is between the limits -> i, j - 1
-		if ((posI >= 0 && posI < table.length) && (posJ - 1 >= 0 && posJ - 1 < table[posJ].length)) {
-			// Check if there is a ship in the position
-			if (table[posI][posJ - 1] == 'S') {
-				valid = false;
-			}
-		}
-
-		// Check if the adjacent position is between the limits -> i, j + 1
-		else if ((posI >= 0 && posI < table.length) && (posJ + 1 >= 0 && posJ + 1 < table[posJ].length)) {
-			// Check if there is a ship in the position
-			if (table[posI][posJ + 1] == 'S') {
-				valid = false;
-			}
-		}
-		// Check if the adjacent position is between the limits -> i - 1, j
-		else if ((posI - 1 >= 0 && posI - 1 < table.length) && (posJ >= 0 && posJ < table[posJ].length)) {
-			// Check if there is a ship in the position
-			if (table[posI - 1][posJ] == 'S') {
-				valid = false;
-			}
-		}
-		// Check if the adjacent position is between the limits -> i - 1, j - 1
-		else if ((posI - 1 >= 0 && posI - 1 < table.length)
-				&& (posJ - 1 >= 0 && posJ - 1 < table[posJ].length)) {
-			// Check if there is a ship in the position
-			if (table[posI - 1][posJ - 1] == 'S') {
-				valid = false;
-			}
-		}
-		// Check if the adjacent position is between the limits -> i - 1, j + 1
-		else if ((posI - 1 >= 0 && posI - 1 < table.length)
-				&& (posJ + 1 >= 0 && posJ + 1 < table[posJ].length)) {
-			// Check if there is a ship in the position
-			if (table[posI - 1][posJ + 1] == 'S') {
-				valid = false;
-			}
-		}
-		// Check if the adjacent position is between the limits -> i + 1, j
-		else if ((posI + 1 >= 0 && posI + 1 < table.length) && (posJ >= 0 && posJ < table[posJ].length)) {
-			// Check if there is a ship in the position
-			if (table[posI + 1][posJ] == 'S') {
-				valid = false;
-			}
-		}
-		// Check if the adjacent position is between the limits -> i + 1, j - 1
-		else if ((posI + 1 >= 0 && posI + 1 < table.length)
-				&& (posJ - 1 >= 0 && posJ - 1 < table[posJ].length)) {
-			// Check if there is a ship in the position
-			if (table[posI + 1][posJ - 1] == 'S') {
-				valid = false;
-			}
-		}
-		// Check if the adjacent position is between the limits -> i + 1, j + 1
-		else if ((posI + 1 >= 0 && posI + 1 < table.length)
-				&& (posJ + 1 >= 0 && posJ + 1 < table[posJ].length)) {
-			// Check if there is a ship in the position
-			if (table[posI + 1][posJ + 1] == 'S') {
-				valid = false;
-			}
-		}
-		
-		if (table[posI][posJ] != 'S') {
-			valid = true;
-		}
-
-		// Return boolean true or false
-		return valid;
-		
-			/* boolean valid = true;
-	        
-	        // Check if position is within bounds
-	        if (posI >= 0 && posI < tablePlayer.length && 
-	            posJ >= 0 && posJ < tablePlayer[0].length) {
-	            
-	            // Check current position
-	            if (tablePlayer[posI][posJ] == 'S') {
-	                valid = false;
-	            }
-	            
-	            // Check adjacent positions
-	            for (int i = Math.max(0, posI - 1); i <= Math.min(tablePlayer.length - 1, posI + 1) && valid; 
-	                 i++) {
-	                for (int j = Math.max(0, posJ - 1); 
-	                     j <= Math.min(tablePlayer[0].length - 1, posJ + 1) && valid; 
-	                     j++) {
-	                    if (tablePlayer[i][j] == 'S') {
-	                        valid = false;
-	                    }
-	                }
-	            }
-	        } else {
-	            valid = false;
-	        }
-	        
-	        return valid; */
-	}
-	
-	public void placeShips(char table[][], char row, int col) {
-		table[row][col] = SHIP;
-	}
-	
-	/**
-	 * Simulates the machine's move by randomly selecting a cell on the player's
-	 * board. Marks the board with 'X' if a ship is hit, 'O' if missed.
-	 *
-	 * @param playerBoard The board where the machine attacks.
-	 * @return true if a ship is hit false otherwise.
-	 */
-	public static boolean machineMove() {
-		Random rand = new Random();
-		int row;
-		int col;
-		boolean res = false;
-
-		do {
-			row = rand.nextInt(playerBoard2.length);
-			col = rand.nextInt(playerBoard2[0].length);
-		} while (playerBoard2[row][col] == HIT || playerBoard2[row][col] == MISS); // Avoid attacking the same place
-
-		if (playerBoard2[row][col] == SHIP) {
-			playerBoard2[row][col] = HIT;
-			res = true; // Ship hit
-		} else {
-			playerBoard2[row][col] = MISS;
-		}
-		
-		return res;
-		
-	}
+        // Check if position is within bounds
+        if (row < 0 || row >= table.length || col < 0 || col >= table[0].length) {
+            return false;
+        }
+        
+        // Check if there's already a ship at this position
+        if (table[row][col] == SHIP) {
+            return false;
+        }
+        
+        // Check all adjacent cells for ships (including diagonals)
+        for (int i = Math.max(0, row - 1); i <= Math.min(table.length - 1, row + 1); i++) {
+            for (int j = Math.max(0, col - 1); j <= Math.min(table[0].length - 1, col + 1); j++) {
+                if (table[i][j] == SHIP) {
+                    return false;
+                }
+            }
+        }
+        
+        // Position is valid
+        return true;
+    }
+    
+    /**
+     * Places a ship at the specified position.
+     *
+     * @param table The board where the ship will be placed
+     * @param rowChar Row character (A, B, C, etc.)
+     * @param col Column number (1-based)
+     */
+    public void placeShips(char[][] table, char rowChar, int col) {
+        int row = rowChar - 'A';
+        col -= 1; // Convert to 0-based indexing
+        
+        if (row >= 0 && row < table.length && col >= 0 && col < table[0].length) {
+            table[row][col] = SHIP;
+        }
+    }
+    
+    /**
+     * Processes a player's shot at the machine's board.
+     *
+     * @param rowChar Row character (A, B, C, etc.)
+     * @param col Column number (1-based)
+     * @return true if a ship was hit, false otherwise
+     */
+    public static boolean playerShoot(char rowChar, int col) {
+        int row = rowChar - 'A';
+        col -= 1; // Convert to 0-based indexing
+        
+        // Check if position is valid
+        if (row < 0 || row >= machineBoard1.length || col < 0 || col >= machineBoard1[0].length) {
+            System.out.println("Invalid coordinates. Try again.");
+            return false;
+        }
+        
+        // Check if position has already been shot
+        if (machineBoard2[row][col] == HIT || machineBoard2[row][col] == MISS) {
+            System.out.println("You already shot at this position. Try again.");
+            return false;
+        }
+        
+        // Process the shot
+        if (machineBoard1[row][col] == SHIP) {
+            machineBoard2[row][col] = HIT;
+            machineBoard1[row][col] = HIT; // Update the machine's board too
+            return true; // Ship hit
+        } else {
+            machineBoard2[row][col] = MISS;
+            machineBoard1[row][col] = MISS; // También actualizar el tablero original con el fallo
+            return false; // Miss
+        }
+    }
+    
+    /**
+     * Checks if all ships on a board have been hit.
+     *
+     * @param board The board to check
+     * @return true if all ships have been hit, false otherwise
+     */
+    public static boolean allShipsHit(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == SHIP) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    /**
+     * Displays all game boards at the end of the game.
+     */
+    public static void showAllBoards() {
+        System.out.println("\n========= PLAYER'S FLEET BOARD =========");
+        tablePaint(playerBoard1);
+        
+        System.out.println("\n========= PLAYER'S SHOTS BOARD =========");
+        tablePaint(machineBoard2);
+        
+        System.out.println("\n========= COMPUTER'S FLEET BOARD =========");
+        tablePaint(machineBoard1);
+        
+        System.out.println("\n========= COMPUTER'S SHOTS BOARD =========");
+        tablePaint(playerBoard2);
+    }
 }
--- src/game/MainBattleship.java
+++ src/game/MainBattleship.java
@@ -4,104 +4,128 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class MainBattleship {
-	
-	static Scanner sc = new Scanner(System.in);
-	
-	static char pos1 = ' ';
-	
-	static int pos2;
 
-	public static void main(String[] args) {
-		
-		Battleship game = new Battleship();
-		
-		int contInicial = 0;
-		
-		int contJugador = 0;
-		int contMaquina = 0;
-		
-		int turno = 1;
-		
-		System.out.println("Wellcome to Battleship Game");
-		
-		System.out.println("First! place your ships in the table (you have 4 ships)");
-		
-		game.initializeBoardWater(game.getPlayerBoard1());
-		game.initializeBoardWater(game.getMachineBoard1());
-		
-		do {
-			
-			System.out.println("This is your table:");
-			game.initializeBoardLines(game.getPlayerBoard2());
-			game.tablePaint(game.getPlayerBoard1());
-			
-			System.out.println("Where you place a ship?:");
-			questionCords();
-			
-			if(game.checksPosition(pos1, pos2, game.getPlayerBoard1())){	
-				game.placeShips(game.getPlayerBoard1(), pos1, pos2);
-				++contInicial;
-			} else {
-				System.out.println("Invalid chosen location, check that the boat is not close to another one");
-			}
-			
-		} while(contInicial != 4);
-		
-		// Colocar los de la maquina (funcion que va a rellenar directamente el tablero con los barcos aleatoriamente colocados)
-		
-		// JUGADOR --> TURNO 1
-		
-		do {
-			
-			if(turno == 1) {
-				
-				System.out.println("Its your turn!");
-				
-				System.out.println("Where you shoot?:");
-				/*MuestraTable()*/
-				questionCords();
-				
-				if(/*DisparaJugador()*/){
-					
-					System.out.println("Nice!! You have sunk a ship");
-					++contJugador;
-				} else {
-					
-					System.out.println("MEH, You shoot water");
-					++turno;
-				}
-				game.tablePaint(game.getPlayerBoard1());
-				
-			} else {
-				
-				if(/*JuegaMaquina()*/){
-					
-					System.out.println("WARNING, Computer has sunk a ship");
-					++contMaquina;
-				} else {
-					System.out.println("WARNING, Computer has sunk a ship");
-					--turno;
-				}
-				
-			}
-			
-		} while(contJugador != 4 || contMaquina != 4);
-		
+    static Scanner sc = new Scanner(System.in);
+    static char pos1 = ' ';
+    static int pos2;
 
-	}
-	
-	private static void questionCords() {
-		System.out.println("X: ");
-		pos1 = sc.next().charAt(0);
-		--pos1;
-		System.out.println("Y: ");
-		pos2 = sc.nextInt();
-		--pos2;
-	}
+    public static void main(String[] args) {
+        Battleship game = new Battleship();
 
-	
-	
-	
-	
+        int contInicial = 0;
+        int contJugador = 0;
+        int contMaquina = 0;
+        int turno = 1;
 
+        System.out.println("Welcome to Battleship Game");
+        System.out.println("First! Place your ships on the table (you have 4 ships)");
+
+        // Initialize all boards
+        game.initializeBoardWater(game.getPlayerBoard1());
+        game.initializeBoardWater(game.getPlayerBoard2());
+        game.initializeBoardWater(game.getMachineBoard1());
+        game.initializeBoardWater(game.getMachineBoard2());
+
+        // Player places their ships
+        do {
+            System.out.println("This is your table:");
+            game.tablePaint(game.getPlayerBoard1());
+
+            System.out.println("Where do you want to place a ship?");
+            questionCords();
+
+            if (Battleship.checksPosition(pos1, pos2, game.getPlayerBoard1())) {
+                game.placeShips(game.getPlayerBoard1(), pos1, pos2);
+                ++contInicial;
+                System.out.println("Ship placed successfully! (" + contInicial + "/4)");
+            } else {
+                System.out.println("Invalid chosen location, check that the boat is not close to another one");
+            }
+
+        } while (contInicial != 4);
+
+        // Computer places its ships
+        System.out.println("Computer is placing its ships...");
+        game.generateBoats(game.getMachineBoard1(), 4);
+        System.out.println("Computer has placed all its ships!");
+
+        // Game starts
+        System.out.println("\nLet the battle begin!");
+        
+        boolean gameOver = false;
+
+        do {
+            if (turno == 1) {
+                // Player's turn
+                System.out.println("\n========= YOUR TURN! =========");
+                System.out.println("Your fleet:");
+                game.tablePaint(game.getPlayerBoard1());
+                System.out.println("\nYour shots:");
+                game.tablePaint(game.getMachineBoard2());
+
+                System.out.println("Where do you want to shoot?");
+                questionCords();
+
+                if (Battleship.playerShoot(pos1, pos2)) {
+                    System.out.println("Nice!! You have sunk a ship!");
+                    ++contJugador;
+                    
+                    if (contJugador == 4) {
+                        System.out.println("Congratulations! You have sunk all enemy ships!");
+                        gameOver = true;
+                    }
+                } else {
+                    System.out.println("You missed! The turn passes to the computer.");
+                    turno = 2;
+                }
+
+            } else {
+                // Computer's turn
+                System.out.println("\n========= COMPUTER'S TURN! =========");
+                
+                if (Battleship.machineMove()) {
+                    ++contMaquina;
+                    
+                    if (contMaquina == 4) {
+                        System.out.println("The computer has sunk all your ships!");
+                        gameOver = true;
+                    }
+                } else {
+                    System.out.println("Computer missed! Your turn now.");
+                    turno = 1;
+                }
+                
+                System.out.println("Your fleet after computer's attack:");
+                game.tablePaint(game.getPlayerBoard1());
+            }
+
+        } while (!gameOver);
+
+        // Game over, show final state
+        System.out.println("\n========= GAME OVER! =========");
+        if (contJugador == 4) {
+            System.out.println("YOU WIN! You sunk all enemy ships!");
+        } else {
+            System.out.println("YOU LOSE! The computer sunk all your ships!");
+        }
+        
+        // Show all boards at the end
+        Battleship.showAllBoards();
+        
+        sc.close();
+    }
+
+    private static void questionCords() {
+        System.out.print("Row (A-J): ");
+        pos1 = sc.next().toUpperCase().charAt(0);
+        
+        System.out.print("Column (1-10): ");
+        pos2 = sc.nextInt();
+        
+        // Validate input
+        if (pos1 < 'A' || pos1 > 'J' || pos2 < 1 || pos2 > 10) {
+            System.out.println("Invalid coordinates! Row must be A-J and column must be 1-10.");
+            questionCords(); // Ask again
+        }
+    }
 }
